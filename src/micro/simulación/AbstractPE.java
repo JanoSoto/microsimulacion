61877749055430e3f3d95e367ac1c02535d071f2
janosoto <alejandro.soto@usach.cl>
2016-10-19 23:59:44 -0300

    Creación de clases de elementos que participan en la simulación bajo el patrón Strategy

---
+++ src/micro/simulación/AbstractPE.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micro.simulación;
+
+/**
+ *
+ * @author JAno
+ */
+public abstract class AbstractPE {
+    
+    private int id;
+    private String nombre;
+    private String next_pe;
+    
+    public AbstractPE(int id, String nombre, String next_pe){
+        this.id = id;
+        this.nombre = nombre;
+        this.next_pe = next_pe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNext_pe() {
+        return next_pe;
+    }
+
+    public void setNext_pe(String next_pe) {
+        this.next_pe = next_pe;
+    }
+    
+    public abstract void sendMessage(double lambda);
+}
---
+++ src/micro/simulación/ProcessingTime.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micro.simulación;
+
+/**
+ *
+ * @author JAno
+ */
+public interface ProcessingTime {
+    public double getProccesingTime(double lambda);
+}
